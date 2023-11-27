fe8c6ae2be9a821c7f2a6b94e1809c3b3b2c53fd
Jeremy Abraham <jeremyabraham17@gmail.com>
2023-11-26 23:12:56 -0500

    small changes to box capture

--- Main.py
+++ Main.py
@@ -20,13 +20,13 @@ while True:
         
     delta_frame = cv2.absdiff(first_frame ,gray_frame_gau)
     
-    thresh_frame = cv2.threshold(delta_frame, 55, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.threshold(delta_frame, 75, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
     
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
     for contour in contours:
-        if cv2.contourArea(contour) < 5000:
+        if cv2.contourArea(contour) < 8000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
--- emailing.py
+++ emailing.py
@@ -2,5 +2,5 @@ import smtplib
 
 
 def send_email():
-
+    print("object has entered and left frame")
 
